ac54724cd7e6ad33cf540a2bcd3070b160b9202e
iavianm <iavianm@mail.ru>
2022-11-24 14:40:31 +0200

    refactor code

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,21 @@
 const page = document.querySelector(".page");
 const editButton = page.querySelector(".profile__edit-button");
 const closeButton = page.querySelector(".popup__close-button-img");
-const formName = page.querySelector(".popup__input-name");
+const formName = page.querySelector(".popup__input_name");
 const profileName = page.querySelector(".profile__name");
-const formDescription = page.querySelector(".popup__input-description");
+const formDescription = page.querySelector(".popup__input_description");
 const profileDescription = page.querySelector(".profile__description");
 const formSubbmit = page.querySelector(".popup__form");
+const openPopup = page.querySelector(".popup");
 
 function editForm() {
-	page.querySelector(".popup").classList.add("popup_opened");
+	openPopup.classList.add("popup_opened");
+	formName.value = profileName.textContent;
+	formDescription.value = profileDescription.textContent;
 }
 
 function closeForm() {
-	page.querySelector(".popup").classList.remove("popup_opened");
-	formName.value = profileName.textContent;
-	formDescription.value = profileDescription.textContent;
+	openPopup.classList.remove("popup_opened");
 }
 
 function formSubmitHandler(evt) {
@@ -27,6 +28,3 @@ function formSubmitHandler(evt) {
 editButton.addEventListener("click", editForm);
 closeButton.addEventListener("click", closeForm);
 formSubbmit.addEventListener("submit", formSubmitHandler);
-
-formName.value = profileName.textContent;
-formDescription.value = profileDescription.textContent;
