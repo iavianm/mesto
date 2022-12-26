79fcbe0b598aabc44dd1429811b25af81ff033dd
iavianm <iavianm@mail.ru>
2022-12-27 00:56:29 +0200

    refactor code and add some functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,3 @@
-/* Тело страницы */
-const page = document.querySelector('.page');
-
-/* Профайл */
-const profile = page.querySelector('.profile');
-const profileEditButton = profile.querySelector('.profile__edit-button');
-const profileAddButton = profile.querySelector('.profile__add-button');
-const profileName = profile.querySelector('.profile__name');
-const profileDescription = profile.querySelector('.profile__description');
-
-/* Попап инфо */
-const infoPopup = page.querySelector('.info-popup');
-const infoPopupCloseButton = infoPopup.querySelector('.info-popup__close-button');
-
-/* Карточки */
-const elements = page.querySelector('.elements');
-
-/* Попап добавления картинки */
-const cardPopup = page.querySelector('.card-popup');
-const cardPopupCloseButton = cardPopup.querySelector('.card-popup__close-button');
-
-/* Попап изображений */
-const imagePopup = page.querySelector('.image-popup');
-const imagePopupPhoto = imagePopup.querySelector('.popup__photo');
-const imagePopupTitle = imagePopup.querySelector('.popup__figcaption');
-const imagePopupCloseButton = imagePopup.querySelector('.image-popup__close-button');
-
-/* Скелет карточки */
-const templateElement = page.querySelector('#element').content;
-
-const formInfo = document.forms.infoPopupForm;
-const infoInputName = formInfo.elements.inputName
-const infoInputDescription = formInfo.elements.inputDescription
-const buttonFormInfo = formInfo.elements.submit
-
-const formCard = document.forms.cardPopupForm;
-const cardInputTitle = formCard.elements.inputTitle
-const cardInputUrl = formCard.elements.inputUrl
-const buttonFormCard = formCard.elements.submit
-
 function editProfileInfo(anyPopup, button) {
   infoInputName.value = profileName.textContent;
   infoInputDescription.value = profileDescription.textContent;
@@ -61,10 +21,31 @@ function addUserCard(anyPopup, button) {
 
 function openPopup(anyPopup) {
   anyPopup.classList.add('popup_opened');
+  document.addEventListener('keydown', closePopupByEscape);
+  document.addEventListener('mouseup', closePopupByOverlay);
+}
+
+function closePopupByEscape(evt) {
+  if (evt.key === 'Escape') {
+    const openPopup = document.querySelector('.popup_opened');
+    const form = openPopup.querySelector('.popup__form')
+    closePopup(openPopup, form);
+  }
+}
+
+function closePopupByOverlay(evt) {
+  if (evt.target.classList.contains('popup_opened')) {
+    const openPopup = document.querySelector('.popup_opened');
+    const form = openPopup.querySelector('.popup__form')
+    closePopup(openPopup, form);
+  }
 }
 
 function closePopup(anyPopup, form) {
   anyPopup.classList.remove('popup_opened');
+
+  document.removeEventListener('keydown', closePopupByEscape);
+  document.removeEventListener('mouseup', closePopupByOverlay);
 
   if (form) {
     form.reset();
